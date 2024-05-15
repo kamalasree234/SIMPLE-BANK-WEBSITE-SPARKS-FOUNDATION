41e0f98b416fe11a8e6e148821aa036423f7cbaa
kamalasree28 <115533946+kamalasree234@users.noreply.github.com>
2024-05-15 20:49:17 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,10 +3,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = "sreeskamala@gmail.com";
-    $subject = "New Contact Form Submission";
-    $body = "Name: $name\nEmail: $email\n\n$message";
-   $headers="From: $email";
     if(!empty($name) && !empty($email) && !empty($message)) {
         echo "<h2>Message sent successfully!</h2>";
     } else {
